3ecb3a49ca6341714cff4ede2cf7816a5208d7cb
bitetiger <89952061+bitetiger@users.noreply.github.com>
2023-03-20 19:57:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-// sendEther.js
 require('dotenv').config()
-
-// const TELEGRAM_TOKEN = process.env.telegram-token;
-// const TELEGRAM_CHAT_ID = "오직 해커를 위함";
-
-// const telegramApi = new TelegramApi(TELEGRAM_TOKEN);
   
   var Web3 = require("web3")
   var fs = require("fs")
   var Tx = require("ethereumjs-tx") // ethereumjs-tx version 1.3.7 사용. (최신 버전을 사용할 경우에는 constructor 관련 에러와 invalid sender 에러가 떠서 버전 수정하였습니다.)
-  var infura_token = process.env.infuraFileToken
+  var rpc_url = process.env.rpcUrl
   var private_key = process.env.privateKey
-  var node_host = `https://ropsten.infura.io/v3/${infura_token}`
   
-  var web3 = new Web3(node_host)
+  var web3 = new Web3(rpc_url)
 
   const send_account = process.env.send_account // send account
   const receive_account = process.env.receive_account //  receiver account
-  const privateKeyBuffer = Buffer.from(private_key, "hex")
+//  const privateKeyBuffer = Buffer.from(private_key, "hex")
+  const privateKeyBuffer = Buffer.from(private_key.slice(2), "hex")
   
   function send(){
   web3.eth.getTransactionCount(send_account, (err, txCount) => { // (1)
@@ -39,7 +33,7 @@ require('dotenv').config()
       .sendSignedTransaction(raw) //(2)
       .once("transactionHash", hash => {
         // telegramApi.sendMessage(TELEGRAM_CHAT_ID, "tx가 pending됨. 트랜잭션 실행되었음");
-        console.info("transactionHash", "https://ropsten.etherscan.io/tx/" + hash) // tx가 pending되는 즉시 etherscan에서 tx진행상태를 보여주는 링크를 제공해줍니다.
+        console.info("transactionHash", + hash)
       })
       .once("receipt", receipt => {
         // telegramApi.sendMessage(TELEGRAM_CHAT_ID, "receipt :" + receipt);
