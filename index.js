97c0fe7cd338959bc334c2f3a94070de9d042a0d
bitetiger <89952061+bitetiger@users.noreply.github.com>
2023-03-28 16:21:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+// sendEther.js
 require('dotenv').config()
-  
+var decimal = require('decimal')
+
   var Web3 = require("web3")
   var fs = require("fs")
   var Tx = require("ethereumjs-tx") // ethereumjs-tx version 1.3.7 사용. (최신 버전을 사용할 경우에는 constructor 관련 에러와 invalid sender 에러가 떠서 버전 수정하였습니다.)
-  var rpc_url = process.env.rpcUrl
-  var private_key = process.env.privateKey
-  
-  var web3 = new Web3(rpc_url)
+  var private_key = '{{ pv_key }}'
+  var node_host = `http://43.201.64.138:8547`
 
-  const send_account = process.env.send_account // send account
-  const receive_account = process.env.receive_account //  receiver account
+  var web3 = new Web3(node_host)
+  var send_value_eth = 1000000
+
+  const send_account =  '{{ }}'// send account
+  const receive_account =  '{{ }}' //  receiver account
 //  const privateKeyBuffer = Buffer.from(private_key, "hex")
   const privateKeyBuffer = Buffer.from(private_key.slice(2), "hex")
-  
+
   function send(){
   web3.eth.getTransactionCount(send_account, (err, txCount) => { // (1)
     const txObject = {
@@ -20,27 +23,26 @@ require('dotenv').config()
       gasLimit: web3.utils.toHex(1000000),
       gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
       to: receive_account,
-      value: "0x00470DE4DF820000", // 0.2 hexcode 변경 
+      value: web3.utils.toHex(web3.utils.toWei(send_value_eth.toString(), "ether"))
     }
-  
+
+    console.log("@@@@@@@@@@ send_value_eth :" + send_value_eth + " @@@@@@@@@@")
     const tx = new Tx(txObject)
     tx.sign(privateKeyBuffer)
-  
+
     const serializedTx = tx.serialize()
     const raw = "0x" + serializedTx.toString("hex")
   
     web3.eth  
       .sendSignedTransaction(raw) //(2)
       .once("transactionHash", hash => {
-        // telegramApi.sendMessage(TELEGRAM_CHAT_ID, "tx가 pending됨. 트랜잭션 실행되었음");
-        console.info("transactionHash", + hash)
+        console.info("transactionHash", "https://ropsten.etherscan.io/tx/" + hash)
       })
       .once("receipt", receipt => {
-        // telegramApi.sendMessage(TELEGRAM_CHAT_ID, "receipt :" + receipt);
         console.info("receipt", receipt) // 터미널에 receipt 출력
       })
-      .on("error", console.error)
+      .on("error", console.error) 
   })
-  }
-
-  setInterval(() => send(), 5000);
+  } 
+    
+  setInterval(() => send(), 2000);
